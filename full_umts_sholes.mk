b5cadb86ce3a32d368166c20890696c7000a7753
Edgard Castro <edgardcastro@gmail.com>
2011-01-06 13:02:00 -0200

    Add gps.conf to build

--- full_umts_sholes.mk
+++ full_umts_sholes.mk
@@ -21,8 +21,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-# This is where we'd set a backup provider if we had one
-#$(call inherit-product, device/sample/products/backup_overlay.mk)
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
 $(call inherit-product, device/motorola/umts_sholes/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
 
